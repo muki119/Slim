3abe172d70a8003002eaf35201ee41adfb376e33
Mk <muki119@users.noreply.github.com>
2021-11-14 16:47:06 +0000

    beggining of dashboard redesign

--- cliside/src/Dashboard/dashboard.js
+++ cliside/src/Dashboard/dashboard.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import {
     Redirect
 } from "react-router-dom";
@@ -8,35 +8,51 @@ import '../general_css/gcss.css';
 
 function Dashboard (props){
     const {urdata,setUser} = useContext(UdContext) // use this to set user data and pull userdata
+    const [logout,setlog] = useState(false)
     document.title = 'Dashboard'
     const dashdata = JSON.parse(localStorage.getItem('UD'))
-    
-    if (dashdata.redirect === false){ // if not authorized 
 
+    function logoutproc(){
+        localStorage.clear()
+        setlog(true)
+    }
+    if (dashdata){
+        if (dashdata.redirect === false){ // if not authorized 
+
+            return(
+                <Redirect push to = '/login' />
+            )
+
+        }else if (dashdata.redirect === true ){ // if there is a redirect allowance 
+            return(
+                <div class = 'dbackground' >
+                    <div class='dinbox'>
+                        <div class ='topbar'>
+                            <span id='barwelcome'>{dashdata.user.firstname} {dashdata.user.surname}</span>
+                            <button id='lout'onClick={logoutproc}>Logout</button>
+                        </div>
+                        <div class = 'chatbar'>
+                            <span><span class= 'chatname'>{dashdata.user.firstname} {dashdata.user.surname}</span></span>
+                            <span>{dashdata.user.surname}</span>
+                        </div>
+
+                    </div>
+                </div>
+            )
+
+        }
+        else if(logout === true) {
+            return(
+                <Redirect push to = '/login' />
+            )
+
+        }  
+    }else{
         return(
             <Redirect push to = '/login' />
         )
-
-    }else if (dashdata.redirect === true ){ // if there is a redirect allowance 
-        return(
-            <div class = 'logbackground dbackground' >
-                <div class='dinbox'>
-                    <div class ='topbar'>
-                        <span id='barwelcome'>{dashdata.user.firstname} {dashdata.user.surname}</span>
-                    </div>
-                    <div class = 'chatbar'>
-                        <span>{dashdata.user.firstname}</span>
-                        <span>{dashdata.user.surname}</span>
-                    </div>
-
-                    <a href = '/register'>register</a>
-                    <p>{JSON.stringify(dashdata.user)}</p>
-
-                </div>
-            </div>
-        )
-
     }
+    
 
 }
  
