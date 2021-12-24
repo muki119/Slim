b59e5695600b4918904de0e6d1aeb4aa1cbe8498
Mk <muki119@users.noreply.github.com>
2021-12-24 23:10:02 +0000

    completed socket integrations - added cleanup functions moved chatroom ui to seperate js doc

--- cliside/src/Dashboard/dashboard.js
+++ cliside/src/Dashboard/dashboard.js
@@ -1,4 +1,4 @@
-import React, { createContext,useContext, useEffect, useState } from 'react';
+import React, { createContext,useContext, useEffect, useState ,useReducer} from 'react';
 import {
     Redirect
 } from "react-router-dom";
@@ -9,6 +9,7 @@ import './dashboard.css';
 import '../general_css/gcss.css';
 import { io } from "socket.io-client" ; 
 import _ from 'lodash';
+import Chatroom from './conversation_component/Chatroom';
 
 
 function Dashboard (props){
@@ -16,38 +17,60 @@ function Dashboard (props){
     const [logout,setlog] = useState(false)
     const [chats,setchats] = useState({data:[]})
     const [currentchatid,changechat] = useState('booom bow') // the id of the 
+    const [socket,setsocket]=useState(null)
+    const [roomidarr,sridarr]  = useState([])
     document.title = 'Dashboard'
-    const dashdata = JSON.parse(localStorage.getItem('UD'))
+    const dashdata = JSON.parse(localStorage.getItem('UD')) // login persistence data
+    const [,forceUpdate] = useReducer(x => x + 1, 0); // force update 
+    
+    function logoutproc(){ // log out process
+        localStorage.clear()
+        socket.disconnect()
+        setlog(true) // do every thing above before this because this redirects to login
+    }
 
-    const socket = io('http://localhost:8210');
+    function chatchanger(e){ // changes the chat 
+        changechat(e.target.id)
+    }
 
- 
-    useEffect(()=>{
-        socket.emit('join_rooms','tme','yooo')
-        console.log('dome')
+    useEffect(()=>{ // establishes ws socket connection and gets all availabele chats of user 
+        const newsocket = io('ws://localhost:8210'); // URL WILL BE FROM .ENV
+        setsocket(newsocket)
+
+        //console.log('dome') 
         async function get_chats(){ // get the user's chats 
             try {
-                var chat= await Axios.post('http://localhost:25565/api/m/getmsgs',{username:dashdata.user.username}) 
+                var chat= await Axios.post('http://localhost:25565/api/m/getmsgs',{username:dashdata.user.username})  // URL WILL BE FROM .ENV+ROUTE
                 //bubble sort the chats by time and get each 
                 setchats(chat)
             } catch (error) {
                 console.log(error)
-            }
-            
+            } 
         }
-        get_chats()
+        get_chats() //calls the get_chats function 
+        return ()=>{newsocket.close()} // once the dashboard closes the socket will be disconnected  - cleanup function
     },[])
- 
-    
-    function logoutproc(){
-        localStorage.clear()
-        socket.close()
-        setlog(true) // do every thing above before this because this redirects to login
-    }
-    function chatchanger(e){
-        changechat(e.target.id)
-    }
 
+    useEffect(()=>{ // if theres chats found that the user is a part of - the chat ids will be gathered and used as seperate socket rooms 
+        if (chats.data !== [] ){
+            chats.data.forEach((e)=>{
+                console.log(e)
+                sridarr(roomidarr=>[...roomidarr,e.chat_id]) 
+            })
+        }  
+        return ()=>{sridarr([])} 
+    },[chats])
+
+    useEffect(()=>{ // joins rooms once everything is established - if the user also has no rooms then the function will not run 
+        if (roomidarr !== [] && socket !== null){
+            socket.emit('join_rooms',roomidarr,'kachow')
+            console.log(roomidarr)
+        }
+    },[roomidarr])
+
+
+
+    
     if (dashdata){
         if (dashdata.redirect === false){ // if not authorized 
 
@@ -56,27 +79,45 @@ function Dashboard (props){
             )
 
         }else if (dashdata.redirect === true ){ // if there is a redirect allowance 
-            var convo = chats.data.map((eh,index)=>{
-                var usersinvolved = [(eh.users_involved.slice(0,eh.users_involved.indexOf(dashdata.user.username))).toString(),(eh.users_involved.slice(eh.users_involved.indexOf(dashdata.user.username)+1)).toString()]
-                //console.log(usersinvolved)
-                return <span key = {index} id= {eh.chat_id} onClick={chatchanger}><span class= 'chatname'>{usersinvolved.map((users)=>{return users+' '})}</span></span>
+
+            
+            const sortalgor = (a,b)=>{
+                //console.table(Date.parse(b.last_messaged))
+                return Date.parse(a.last_messaged)-Date.parse(b.last_messaged)
+            }
+            var sk  = chats.data
+            sk.sort(sortalgor) // sorts the chats by last messaged 
+            sk.reverse()
+            
+            var convomp = chats.data.map((eh,index)=>{
+                var usersinvolved = [(eh.users_involved.slice(0,eh.users_involved.indexOf(dashdata.user.username))).toString(),(eh.users_involved.slice(eh.users_involved.indexOf(dashdata.user.username)+1)).toString()] // removes the users name from the available recipients list 
+                return <span key = {index} id={eh.chat_id} onClick={chatchanger}><span class= 'chatname'>{usersinvolved.map((users)=>{return users+' '})}</span></span> // id for the chat_id used -chatchanger is a function that changes the conversation by making the new one a 
             })
+
             return(
                 <div class = 'dbackground' >
                     <div class='dinbox'>
+
                         <div class ='topbar'>
-                            <span id='barwelcome'>{dashdata.user.firstname.charAt(0).toUpperCase()+dashdata.user.firstname.slice(1)} {dashdata.user.surname} ( {dashdata.user.username} )</span>
+
+                            <span id='barwelcome'>{dashdata.user.firstname.charAt(0).toUpperCase()+dashdata.user.firstname.slice(1)} {dashdata.user.surname} ( {dashdata.user.username} )</span> 
                             <button id='lout'onClick={logoutproc}>Logout</button>
+                            
                         </div>
+
                         <div class = 'chatandbar'>
+
                             <div class = 'chatbar'>
                                 <span><span class= 'chatname'>{dashdata.user.firstname} {dashdata.user.surname}</span></span>
-                                {convo}
+                                {convomp}
                             </div>
+
                             <div class = 'openchat'>
-                                <Messagecontext.Provider value = {{chats,setchats,currentchatid}}>
+
+                                <Messagecontext.Provider value = {{chats,setchats,currentchatid,socket,setsocket,forceUpdate}}>
                                     <Chatroom/>
                                 </Messagecontext.Provider>
+
                             </div>
                         </div>
                     </div>
@@ -100,19 +141,4 @@ function Dashboard (props){
 }
  
 
-function Chatroom(){
-    const {chats,setchats,currentchatid} = useContext(Messagecontext)
-    const p = chats.data.findIndex((e)=>{return e.chat_id == currentchatid})
-    const currentchat = chats.data[p] // change the contents using setchats
-
-    console.log(p)
-    return (
-
-
-        <div>
-            <div class = 'top'>{JSON.stringify(p)}</div>
-            <div>{JSON.stringify(currentchat)}</div>
-        </div>
-    )
-}
 export default Dashboard
