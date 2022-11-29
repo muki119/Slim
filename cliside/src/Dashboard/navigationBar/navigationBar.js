7e3a40a043b3da511dd4d3f760ab76fad73226d3
Mugagga Kimera <mugagga.kimera04@gmail.com>
2022-11-29 21:47:08 +0000

    changed the topbar item positioning

--- cliside/src/Dashboard/navigationBar/navigationBar.js
+++ cliside/src/Dashboard/navigationBar/navigationBar.js
@@ -6,56 +6,61 @@ import DevicesIcon from '@mui/icons-material/Devices';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Avatar ,Tooltip, IconButton, Menu,MenuItem,ListItemIcon, Divider} from '@mui/material';
 export default function NavigationBar({setOpenMenu, dashdata, openMenu, setcurrentThemeFunc, currentTheme, logoutProcess,opencb,openChatbar}) {
-    return <nav className='topbar'>
+    return (
+    <nav className='topbarcontainer'>
         {/*<span id='barwelcome'>{dashdata.user.firstname.charAt(0).toUpperCase()+dashdata.user.firstname.slice(1)} {dashdata.user.surname} ( {dashdata.user.username} )</span> */}
         {/*<button tabIndex={0} id='logout'onClick={logoutProcess}><LogoutIcon/>Logout</button>*/}
-        <Tooltip title={openChatbar?'Close Menu':'Open Menu'}>
-            <IconButton disableRipple={true} onClick={opencb}>
-                <MenuIcon className='menuicon'/>
-            </IconButton>
-        </Tooltip>
-
-        <Tooltip title="Account Settings">
-            <IconButton disableRipple={true} onClick={(e) => { setOpenMenu(e.currentTarget); } }>
-                <Avatar className='avatar'>{dashdata.user.firstname.charAt(0).toUpperCase()}{dashdata.user.surname.charAt(0).toUpperCase()}</Avatar>
-            </IconButton>
-        </Tooltip>
-
-        <Menu id='Menu' anchorEl={openMenu} open={Boolean(openMenu)} onClose={(e) => { setOpenMenu(false); } } >
-            <Tooltip title="Copy Username">
-                <MenuItem onClick={() => { navigator.clipboard.writeText(dashdata.user.username); } }>
-                    {dashdata.user.username}
-                </MenuItem>
+        <div className='topbar'>
+            <Tooltip title={openChatbar?'Close Menu':'Open Menu'}>
+                <IconButton disableRipple={true} onClick={opencb}>
+                    <MenuIcon className='menuicon'/>
+                </IconButton>
             </Tooltip>
 
-            <Divider />
+            <Tooltip title="Account Settings">
+                <IconButton disableRipple={true} onClick={(e) => { setOpenMenu(e.currentTarget); } }>
+                    <Avatar className='avatar'>{dashdata.user.firstname.charAt(0).toUpperCase()}{dashdata.user.surname.charAt(0).toUpperCase()}</Avatar>
+                </IconButton>
+            </Tooltip>
 
-            <MenuItem onClick={setcurrentThemeFunc}>
-                <ListItemIcon>
-                    {(currentTheme === "light") ? <DarkModeOutlinedIcon className='lightanddarkicons' /> :
-                        <> 
-                            {(currentTheme === "null")?
-                                <LightModeIcon className='lightanddarkicons' />
-                                    :
-                                <DevicesIcon  className='lightanddarkicons' />
-                            }
-                        </>
-                    }
-                </ListItemIcon>
-                {(currentTheme === "light") ? <>Toggle Dark Theme</> : <>{(currentTheme === "null")?<>Toggle Light Theme</>:<>Use Device Theme</> }</>  }
-            </MenuItem>
+            <Menu id='Menu' anchorEl={openMenu} open={Boolean(openMenu)} onClose={(e) => { setOpenMenu(false); } } >
+                <Tooltip title="Copy Username">
+                    <MenuItem onClick={() => { navigator.clipboard.writeText(dashdata.user.username); } }>
+                        {dashdata.user.username}
+                    </MenuItem>
+                </Tooltip>
 
-            <Divider />
+                <Divider />
 
-            <MenuItem onClick={logoutProcess}>
-                <ListItemIcon>
-                    <LogoutIcon id="logout-icon" fontSize="medium" />
-                </ListItemIcon>
-                Logout
-            </MenuItem>
+                <MenuItem onClick={setcurrentThemeFunc}>
+                    <ListItemIcon>
+                        {(currentTheme === "light") ? <DarkModeOutlinedIcon className='lightanddarkicons' /> :
+                            <> 
+                                {(currentTheme === "null")?
+                                    <LightModeIcon className='lightanddarkicons' />
+                                        :
+                                    <DevicesIcon  className='lightanddarkicons' />
+                                }
+                            </>
+                        }
+                    </ListItemIcon>
+                    {(currentTheme === "light") ? <>Toggle Dark Theme</> : <>{(currentTheme === "null")?<>Toggle Light Theme</>:<>Use Device Theme</> }</>  }
+                </MenuItem>
 
-        </Menu>
+                <Divider />
+
+                <MenuItem onClick={logoutProcess}>
+                    <ListItemIcon>
+                        <LogoutIcon id="logout-icon" fontSize="medium" />
+                    </ListItemIcon>
+                    Logout
+                </MenuItem>
+
+            </Menu>
 
 
-    </nav>;
+        </div>
+        
+
+    </nav>)
 }
